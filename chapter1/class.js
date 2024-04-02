ed9015f849d76af34d028a2842feb2efc93d0e91
akm4545 <akm4545@naver.com>
2024-04-02 17:32:13 +0900

    chapter1

--- chapter1/class.js
+++ chapter1/class.js
@@ -159,6 +159,9 @@
 {
     // 정적 메서드
     // 클래스의 인스턴스가 아닌 이름으로 호출
+    // 정적 메서드 내부의 this는 클래스로 생성된 인스턴스가 아닌 클래스 자신을 가리킴
+    // 메서드에서 일반적으로 사용하는 this 사용 불가능
+    // 애플리케이션 전역에서 사용하는 유틸 함수를 정적 메서드로 많이 활용하는 편
     class Car {
         static hello(){
             console.log('안녕하세요!')
@@ -169,4 +172,144 @@
 
     myCar.hello() //error
     Car.hello() //안녕하세요!
+
+    // 리액트 클래스 컴포넌트 생명주기인 static getDerivedStateFromProps(props, state)에서는 this.state에 접근할 수 없다
+}
+
+{
+    // 상속
+    // extends로 기존 클래스를 상속받아서 확장
+    class Car {
+        constructor(name){
+            this.name = name
+        }
+
+        honk(){
+            console.log(`${this.name} 경적을 울립니다!`)
+        }
+    }
+
+    class Truck extends Car{
+        constructor(name){
+            //부모 클래스의 constructor (Car constructor)
+            super(name)
+        }
+
+        load(){
+            console.log('짐을 싣습니다.')
+        }
+    }
+
+    const myCar = new Car('자동차')
+    myCar.honk() //자동차 경적을 울립니다!
+
+    const truck = new Truck('트럭')
+    truck.honk() //트럭 경적을 울립니다!
+    truck.load() //짐을 싣습니다!
+}
+
+{
+    // ES6 이전 = 프로토타입을 활용해 클래스의 작동 방식을 동일하게 구현
+    // 클래스 작동 방식 = 자바스크립트의 프로토타입 활용
+
+    // Car클래스 코드 바벨 트랜스파일 결과
+    // 클래스 코드
+    class Car {
+        constructor(name){
+            this.name = name
+        }
+
+        honk(){
+            console.log(`${this.name}이 경적을 울립니다!`)
+        }
+
+        static hello(){
+            console.log('저는 자동차입니다')
+        }
+
+        set age(value){
+            this.carAge = value;
+        }
+
+        get age(){
+            return this.carAge;
+        }
+    }
+
+    // 바벨 변환 코드
+    'use strict'
+
+    //클래스가 함수처럼 호출되는 것을 방지
+    function _classCallCheck(instance, Constructor){
+        if(!(instance instanceof Constructor)){
+            throw new TypeError('Cannot call a class as a function')
+        }
+    }
+
+    // 프로퍼티 할당 코드
+    function _definePropertes(target, props){
+        for (var i = 0; i<props.length; i++){
+            var descriptor = props[i]
+
+            descriptor.enumerable = descriptor.enumerable || false
+            descriptor.configurable = true
+
+            if('value' in descriptor) descriptor.writable = true
+            Object.defineProperty(target, descriptor.key, descriptor)
+        }
+    }
+
+    // 프로토타입 메서드와 정적 메서드를 선언하는 코드
+    // 헬퍼 함수를 만들어 클래스와 동일한 방식으로 동작할 수 있도록 변경
+    function _createClass(Constructor, protoProps, staticProps){
+        if(protoProps) _definePropertes(Constructor.prototype, protoProps)
+        if(staticProps) _definePropertes(Constructor, staticProps)
+        Object.defineProperty(Constructor, 'prototype', {writable: false})
+
+        return Constructor
+    }
+
+    var Car = /*#__PURE__*/ (function(){
+        function Car(name){
+            _classCallCheck(this, Car)
+
+            this.name = name
+        }
+
+        _createClass(
+            Car,
+            [
+                {
+                    key: 'honk',
+                    value: function honk(){
+                        console.log(
+                            ''.concat(
+                                this.name,
+                                '\uC774 \uACBD\uC801\uC744 \uC6B8\uB9BD\uB2C8\uB2E4!',
+                            ),
+                        )
+                    },
+                },
+                {
+                    key: 'age',
+                    get: function get(){
+                        return this.carAge
+                    },
+                    set: function set(value){
+                        this.carAge = value
+                    },
+                },
+            ],
+            [
+                {
+                    key: 'hello',
+                    value: function hello(){
+                        console.log('저는 자동차입니다')
+                    },
+                },
+            ],
+        )
+
+        return Car
+    })()
 }

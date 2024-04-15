3759a3a9de2298bbab2ab27d487b5f2a092f99ec
akm4545 <akm4545@naver.com>
2024-04-15 16:00:47 +0900

    chapter2

--- chapter2/class_and_functional_component.ts
+++ chapter2/class_and_functional_component.ts
@@ -163,3 +163,168 @@
     }
 }
 
+{
+    // 생명주기 메서드
+    // 실행 시점 (크게 3가지 분류)
+    // 마운트(mount): 컴포넌트 마운팅(생성) 시점
+    // 업데이트(update): 이미 생성된 컴포넌트의 내용 변경
+    // 언마운트(unmount): 컴포넌트가 더 이상 존재하지 않는 시점
+
+    // render()
+    // 생명주기 메서드, 리액트 클래스 컴포넌트의 유일한 필수 값
+    // UI를 렌더링하기 위해 쓰이며 마운트와 업데이트 과정에서 일어남
+    // render함수는 항상 순수해야 하며 부수 효과가 없어야 한다
+    // 입력값(props, state)가 들어가면 항상 같은 결과물을 반환해야 한다
+    // 따라서 render() 내부에서 state를 직접 업데이트 하는 this.setState를 호출해서는 안 된다
+    // state를 변경하는 일은 클래스 컴포넌트의 메서드나 다른 생명주기 메서드 내부에서 발생해야 한다
+    // 그러므로 항상 최대한 간결하고 깔끔하게 작성하는 것이 좋다
+
+    // componentDidMount()
+    // 클래스 컴포넌트가 마운트되고 준비가 됐다면 그 다음 호출
+    // 마운트되고 준비되는 즉시 실행
+    // this.setState()로 state값 변경 가능 -> 호출 시 state가 변경되고 그 즉시 다시 한번 렌더링 시도 -> 브라우저가 실제 UI를 업데이트 하기 전에 실행되어 사용자는 변경을 눈치챌 수 없음
+    // componentDidMount는 성능 문제를 일으킬 수 있다 
+    // 일반적으로 state를 다루는 것은 생성자에서 하는것이 좋다
+    // this.setState 사용은 생성자에서 할 수 없는 것, API 호출 후 업데이트, DOM에 의존적인 작업(이벤트 리스너 추가 등)등을 하기 위함
+
+    // componentDidUpdate()
+    // 컴포넌트 업데이트 이후 실행
+    // 일반적으로 state, props의 변화에 따라 DOM을 업데이트하는 등에 쓰임
+    // this.setState를 호출할 수 있으나 적절한 조건문으로 감싸지 않으면 계속 호출 -> 성능적인 문제 발생
+    componentDidUpdate(prevProps: Props, prevState: State){
+        // 만약 이러한 조건문이 없다면 props가 변경되는 매 순간마다 fetchData가 실행
+        // 이 조건문 덕분에 props의 userName이 이전과 다른 경우에만 호출
+        if(this.props.userName !== prevProps.userName){
+            this.fetchData(this.props.userName);
+        }
+    }
+
+    // componentWillUnmount()
+    // 컴포넌트 언마운트 혹은 더 이상 사용되지 않기 직전에 호출
+    // 메모리 누수나 불필요한 작동을 막기 위한 클린업 함수 호출
+    // this.setState호출 불가
+    // 아래처럼 이벤트를 지우거나 API호출을 취소, setInterval, setTimeout으로 생성된 타이머를 지우는 등의 작업을 하는 데 유용
+    componentWillUnmount(){
+        window.removeEventListener('resize', this.resizeListener)
+        clearInterval(this.intervalId)
+    }
+
+    //shouldComponentUpdate()
+    // state, props의 변경으로 리액트 컴포넌트가 다시 리렌더링되는 것을 막고 싶다면 해당 생명주기 메서드를 사용
+    // 컴포넌트에 영향을 받지 않는 변화에 대해 정의할 수 있다
+    // state의 변화에 따라 리렌더링되는 것은 굉장히 자연스러운 일이므로 이 메서드를 사용하는 것은 특정한 성능 최적화 상황에서만 고려해야 한다
+    shouldComponentUpdate(nextProps: Props, nextState: State){
+        // true인 경우 컴포넌트 업데이트 
+        return this.props.title !== nextProps.title || this.state.input !== newState.input
+    }
+}
+
+{
+    // 컴포넌트의 두 가지 유형 (Component, PureComponent)의 차이
+    // 생명주기를 다루는 차이점이 존재
+
+    // 예제
+    import React from 'react'
+
+    interface State{
+        count: number
+    }
+
+    type Props = Record<string, never>
+
+    export class ReactComponent extends React.Component<Props, State> {
+        private renderCounter = 0
+
+        private constructor(props: Props){
+            super(props)
+
+            this.state = {
+                count: 1,
+            }
+        }
+
+        private handleClick = () => {
+            this.setState({count: 1})
+        }
+
+        public render(){
+            console.log('ReactComponent', ++this.renderCounter)
+
+            return (
+                <h1>
+                    ReactComponent: {this.state.count}{''}
+                    <button onClick={this.handleClick}>+</button>
+                </h1>
+            )
+        }
+    }
+
+    export class ReactPureComponent extends React.PureComponent<Props, State> {
+        private renderCounter = 0
+
+        private constructor(props: Props){
+            super(props)
+
+            this.state = {
+                count: 1,
+            }
+        }
+
+        private handleClick = () => {
+            this.setState({count: 1})
+        }
+
+        public render(){
+            console.log('ReactPureComponent', ++this.renderCounter)
+
+            return (
+                <h1>
+                    ReactPureComponent: {this.state.count}{' '}
+                    <button onClick={this.handleClick}>+</button>
+                </h1>
+            )
+        }
+    }
+
+    export default function CompareComponent(){
+        return (
+            <>
+                <h2>React.Component</h2>
+                <ReactCompoent />
+                <h2>React.PureComponent</h2>
+                <ReactPureComponent />
+            </>
+        )
+    }
+
+    // 버튼을 누르면 count를 1로 다시 세팅
+    // Component의 경우 버튼을 누르느 대로 state가 업데이트 되어 렌더링 (비교 수행 x)
+    // PureComponent는 state의 값이 업데이트되지 않아 렌더링 x (얕은 비교를 수행해 결과가 다를 때만 렌더링 수행)
+    // PureComponent는 객체와 같이 복잡한 구조의 데이터 변경은 감지를 못한ㄴ다
+    // PureComponen로만 구성돼 있다면 오히려 성능에 역효과를 미칠 수도 있다
+    // 얕은 비교를 수행 시 일치하지 않는 일이 더 잦다면 이러한 비교는 무의미 하기 떄문이다
+}
+
+{
+    // static getDerivedStateFromProps()
+    // 가장 최근에 도입된 생명주기 메서드 중 하나
+    // 사라진 componentWillReceiveProps를 대체
+    // render()를 호출하기 직전에 호출
+    // static으로 선언하여 this에 접근할 수 없다
+    // 반환 객체는 해당 객체의 내용이 모두 state로 들어간다 
+    // null 반환 시 아무 일도 일어나지 않는다
+    // 모든 render() 실행 시에 호출
+    static getDerivedStateFromProps(nextProps: Props, prevState: State){
+        // 다음에 올 props를 바탕으로 현재의 state를 변경하고 싶을 때 사용할 수 있다
+        if(props.name !== state.name){
+            //state 변경
+            return {
+                name: props.name
+            }
+        }
+
+        // state에 영향을 미치지 않는다
+        return null
+    }
+}
+

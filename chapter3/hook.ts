cdffff18ad70e269ffc7723364b87a589cff6125
akm4545 <akm4545@naver.com>
2024-04-18 11:24:00 +0900

    chapter3

--- chapter3/hook.ts
+++ chapter3/hook.ts
@@ -1,3 +1,59 @@
 {
-    
+    // 훅은 클래스 컴포넌트에서만 가능했던 state.ref 등 리액트 핵심적인 기능을 함수에서도 가능하게 만들었고
+    // 무엇보다 클래스 컴포넌트보다 간결하게 작성할 수 있다
+}
+
+{
+    // useState
+    // 함수 컴포넌트 내부에서 상태를 정의하고 이 상태를 관리할 수 있게 해주는 훅
+
+    // useState의 인수로 사용할 state 초기값 전달 
+    // 아무런 값을 넘겨주지 않으면 undefined다 
+    // useState 훅의 반환 값은 배열이며 배열의 첫 번쨰 요소로 state 값 자체를 사용할 수 있고 
+    // 두 번째 원소인 setState 함수를 사용해 해당 state의 값을 변경할 수 있다
+    import {useState} from 'react'
+
+    const [state, setState] = useState(initalState)
+}
+
+{
+    // useState를 사용하지 않고 함수 내부에서 자체적으로 변수를 사용해 상태값 관리 
+    // 아래 코드는 리렌더링을 발생시키기 위한 조건을 전혀 충족하지 못한다
+    function Component(){
+        let state = 'hello'
+
+        function handleButtonClick(){
+            state = 'hi'
+        }
+
+        return (
+            <>
+                <h1>{state}</h1>
+                <button onClick={handleButtonClick}>hi</button>
+            </>
+        )
+    }
+}
+
+{
+    // useState 반환값의 두 번째 원소를 실행해 리액트에서 렌더링이 일어나게끔 변경했다
+    // 하지만 변경된 값이 렌더링 되진 않는데 매번 렌더링이 발생될 때마다 함수는 다시 새롭게 실행되고
+    // 새롭게 실행되는 함수에서 state는 매번 hello로 초기화 되므로 state를 변경해도 다시 hello 로 초기화
+    function Component(){
+        const [, triggerRender] = useState()
+
+        let state = 'hello'        
+
+        function handleButtonClick() {
+            state = 'hi'
+            triggerRender()
+        }
+
+        return(
+            <>
+                <h1>{state}</h1>
+                <button onClick={handleButtonClick}>hi</button>
+            </>
+        )
+    }
 }

e0cb7dcccc870126e67f8dc3517a96e46700dbea
akm4545 <akm4545@naver.com>
2024-04-19 14:40:29 +0900

    chapter3

--- chapter3/hook.ts
+++ chapter3/hook.ts
@@ -309,4 +309,169 @@
             </>
         )
     }
+
+    // 실행 결과
+    // 클린업 함수 실행! 0
+    // 1
+    // 클린업 함수 실행! 1
+    // 2
+    // 클린업 함수 실행! 2
+    // 3
+    // 클린업 함수 실행! 2
+    // 4
+    
+    // 클린업 함수는 이전 state를 참조해 실행된다
+    // 클린업 함수는 새로운 값과 함께 렌더링된 뒤에 실행
+    // 새로운 값을 기반으로 렌더링 뒤에 실행되지만 이 변경된 값을 읽는 것이 아니라 함수가 정의됐을 당시에 선언됐던 이전 값을 보고 실행
+}
+
+{
+    // 이러한 사실을 코드로 직관적으로 표현
+    // 렌더링이 수행될 때마다 counter가 어떤 값을 선언돼 있는지 보여준다
+    
+    // 최초 실행
+    useEffect(() => {
+        function addMouseEvent(){
+            console.log(1)
+        }
+
+        window.addEventListener('click', addMouseEvent)
+
+        //클린업 함수
+        //다음 렌더링이 끝난 뒤에 실행
+        return () => {
+            console.log('클린업 함수 실행!', 1)
+            window.removeEventListener('click', addMouseEvent)
+        }
+    }, [counter])
+    //
+    //...
+
+    // 이후 실행
+    useEffect(() => {
+        function addMouseEvent(){
+            console.log(2)
+        }
+
+        window.addEventListener('click', addMouseEvent)
+
+        // 클린업 함수
+        return () => {
+            console.log('클린업 함수 실행!', 2)
+            window.removeEventListener('click', addMouseEvent)
+        }
+    }, [counter])
+
+    // 함수 컴포넌트의 useEffect는 그 콜백이 실행될 때마다 이전의 클린업 함수가 존재한다면 그 클린업 함수를
+    // 실행한 뒤에 콜백을 실행한다
+    // 따라서 이벤트를 추가하기 전에 이전에 등록했던 이벤트 핸들러를 삭제하는 코드를 클린업 함수에 추가하는 것이다
+    // 이렇게 함으로써 특정 이벤트의 핸들러가 무한히 추가되는 것을 방지할 수 있다
+    // 생명주기 언마운트 개념과는 차이가 있고 리렌더링됐을 때 의존성 변화가 있었을 당시 이전의 값을 기준으로 실행되는
+    // 말 그대로 이전 상태를 청소해 주는 개념
+}
+
+{
+    // 의존성 배열
+    // 빈 배열을 둔다면 리액트가 해당 useEffect는 비교할 의존서이 없다고 판단해 최초 렌더링 직후에 실행된 다움부터는
+    // 실행되지 않는다
+    // 아무 값도 넘겨주지 않는다면 의존성을 비교할 필요 없이 렌더링시 실행이 필요하다고 판단해 매번 실행된다
+    // 이는 보통 컴포넌트가 렌더링됐는지 확인하기 위한 방법으로 사용
+    useEffect(() => {
+        console.log('컴포넌트 렌더링됨')
+    })
+
+    // 의존성 배열이 없는 useEffect 사용과 그냥 useEffect 없이 사용의 차이점
+    //1
+    function Component(){
+        console.log('렌더링됨')
+    }
+
+    //2 
+    function Component(){
+        useEffect(() => {
+            console.log('렌더링됨')
+        })
+    }
+
+    // 차이점
+    // 1. 서버 사이드 렌더링 관점에서 useEffect는 클라이언트 사이드에서 실행을 보장
+    // useEffect 내부에서는 window 객체의 접근에 의존하는 코드 사용 가능
+    // 2. useEffect는 컴포넌트 렌더링의 부수 효과 즉 컴포넌트의 렌더링이 완료된 이후에 실행
+    // 반면 1번 코드는 컴포넌트가 렌더링되는 도중에 발생
+    // 따라서 2번 코드와 달리 서버 사이드 렌더링의 경우에 서버에서도 실행
+    // 해당 작업은 함수 컴포넌트의 반환을 지연시키는 행위이다 즉 무거운 작업일 경우 렌더링을 방해하므로 성능에 악영향을 미칠 수 있다
+}
+
+{
+    // useEffect 구현
+    // 리액트 코드를 직접 구현할 수는 없다 대략적인 모습을 다음과 같이 상상해 볼 수 있다
+    const MyReact = (function () {
+        const global = {}
+        let index = 0
+
+        function useEffect(callback, dependencies){
+            const hooks = global.hooks
+
+            // 이전 훅 정보가 있는지 확인
+            let previouseDependencies = hooks[index]
+
+            // 변경됐는지 확인
+            // 이전 값이 있다면 얕은 비교로 변경 체크
+            // 이전 값이 없다면 최초 실행이므로 변경이 일어난 것으로 간주해 실행을 유도
+            let isDependenciesChanged = previouseDependencies
+                ? dependencies.some(
+                    (value, idx) => !Object.is(value, previouseDependencies[idx]),
+                )
+                : true
+
+            // 변경이 일어났다면 첫 번째 인수인 콜백 함수 실행
+            if(isDependenciesChanged){
+                callback()
+
+                // 다음 훅이 일어날 때를 대비하기 위해 index 추가
+                index++
+
+                // 현재 의존성을 훅에 다시 저장
+                hooks[index] = dependencies
+            }
+
+            return {useEffect}
+        }
+    })()
+}
+
+{
+    // useEffect 주의점
+    // eslint-disable-line react-hooks/exhaustive-deps 주석은 최대한 자제하라
+    // eslint-disable-line, react-hooks/exhaustive-deps 주석을 사용해서 ESLink의 react-hooks/exhaustive-deps 룰에서
+    // 발생하는 경고를 무시하는것을 볼 수 있다
+    // 이 ESLinkt룰은 useEffect 인수 내부에서 사용하는 값 중 의존성 배열에 포함돼 있지 않은 값이 있을 때 경고를 발생시킨다
+    useEffect(() => {
+        console.log(props)
+    }, []) // 경고
+
+    // 대부분의 경우 의도치 못한 버그를 만들 가능성이 높은 코드
+    // 해당 코드를 대부분 컴포넌트 마운트 시점에만 무언가를 하고 싶다라는 의도로 작성하곤 한다
+    // 그러나 이는 클래스 컴포넌트의 componentDidMount에 기반한 접근법으로 가급적이면 사용해선 안된다
+    // 컴포넌트의 state, props와 같은 어떤 값의 변경과 useEffect의 부수 효과가 별개로 작동하게 된다
+    // useEffect 내부의 콜백 함수의 ㅅㄹ행과 내부에서 사용한 값의 실제 변경 사이에 연결 고리가 끊어진다
+
+    // 정말로 의존성으로 []가 필요하다면 최초에 함수 컴포넌트가 마운트 시점에만 콜백 함수 실행이 필요한기 생각해봐야 한다
+    // 만약 그렇다라면 useEffect 내 부수 효과가 실행될 위치가 잘못됐을 가능성이 크다
+
+    // 아래 코드는 log가 최초로 props로 넘어와 컴포넌트가 최초로 렌더링된 시점에만 실행
+    // 당장은 문제가 없을지라도 log가 아무리 변해도 부수 효과는 실행되지 않는다
+    // useEffect의 흐름과 컴포넌트의 props.log 흐름이 맞지 않게 된다
+    function Component({log} : {log: string}){
+        useEffect(() => {
+            logging(log) //eslint-disable-line react-hooks/exhaustive-deps
+        }, [])
+    }
+
+    // 앞의 logging 이라는 작업은 log를 props로 전달하는 부모 컴포넌트에서 실행되는 것이 옳을지도 모른다
+    // 부모 컴포넌트에서 Component가 렌더링되는 시점을 결정하고 이에 맞게 log값을 넘겨준다면 useEffect의 해당 주석을 제거해도
+    // 위 예제 코드와 동일한 결과를 만들 수 있고 Component의 부수 효과 흐름을 거스르지 않을 수 있다
+
+    // 빈 배열이 아닐때도 만약 특정 값을 사용하지만 해당 값의 변경 시점을 피할 목적이라면 메모이제이션을 적절히 활용해
+    // 해당 값의 변화를 막거나 적당한 실행 위치를 다시 한번 고민해보는 것이 좋다
 }

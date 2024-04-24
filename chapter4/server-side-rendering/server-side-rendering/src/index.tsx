99b4cf1a9783fe59551327dc65e76c72ead2aed1
akm4545 <akm4545@naver.com>
2024-04-24 11:26:39 +0900

    chapter4

--- chapter4/server-side-rendering/server-side-rendering/src/index.tsx
+++ chapter4/server-side-rendering/server-side-rendering/src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
 
 import { hydrate } from 'react-dom';
 import {fetchTodo} from './fetch';
@@ -17,6 +15,9 @@ import {fetchTodo} from './fetch';
 // 서버에서 완성한 HTML과 하이드레이션 대상이 되는 HTML의 결과물이 동일한지 비교하는 작업을 거치므로 이 비교작업을 무사히 수행하기 위해
 // 한 번 더 데이터 조회
 async function main(){
+
+  // fetch를 두 번 호출하고 있는데 결과물이 변경되지 않기 때문에 크게 문제는 없지만 대부분의 API는 호출할 때마다 결과가 변경될 수 있다
+  // 렌더링 구현에만 초점을 맞췄기 때문에 해당 처리가 생략
   const result = await fetchTodo()
 
   const app = <App todos={result} />

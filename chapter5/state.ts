e4e3c70c73c9767d5911790fd23e7bdb0aa2996b
akm4545 <akm4545@naver.com>
2024-04-25 18:24:49 +0900

    chapter5

--- chapter5/state.ts
+++ chapter5/state.ts
@@ -100,5 +100,49 @@
     // 웹페이지를 선언적으로 작성하기 위한 언어
 
     // Elm을 이용해 HTML을 작성한 예제
+    module Main exposing (..)
+
+    import Browser
+    import Html exposing (Html, button, div, text)
+    import Html.Events exposing (onClick)
+
+    -- MAIN
+    main =
+        Browser.sandbox { init = init, update = update, view = view }
+
+    -- MODEL
+    type alias Model = Int
+
+    init : Model
+    init = 
+        0
+
+    -- UPDATE
+    type Msg
+        = Increment
+        | Decrement
     
+    update : Msg -> Model -> Model
+    update msg model =
+        case msg of
+            Increment ->
+                model + 1
+            
+            Decrement ->
+                model - 1
+    
+    -- VIEW
+
+    view : Model -> Html Msg
+    view model = 
+        div []
+            [ button [ onClick Decrement ] [ text "-" ]]
+            , div [] [ text (String.fromInt model) ]
+            , button [ onClick Increment ] [ text "+" ]
+            ]
+    <div>
+        <button>-</button>
+        <div>2</div>
+        <button>+</button>
+    </div>
 }

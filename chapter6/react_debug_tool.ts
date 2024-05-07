c527b6a1d3308960681baeec03d5502854a29410
akm4545 <akm4545@naver.com>
2024-05-07 12:28:09 +0900

    chapter6

--- chapter6/react_debug_tool.ts
+++ chapter6/react_debug_tool.ts
@@ -125,3 +125,79 @@
     // 그러므로 displayName과 함수명은 개발 모드에서만 제한적으로 참고하는 것이 좋다
 }
 
+{
+    // Components 오른쪽 영역은 해당 컴포넌트의 자세한 정보를 보여주는 영역
+}
+
+{
+    // 컴포넌트 명 key="키값"
+    // Anonymous key="kidsValueProp"
+    // 을 클릭하면 오른쪽 화면에 
+    // kidsValueProp Anonymous가 출력
+    // 경고 표시는 해당 애플리케이션이 strict mode로 렌더링되지 않았다는것을 의미
+}
+
+{
+    // 컴포넌트 오른쪽 상단의 3개 아이콘
+
+    // 눈
+    // 해당 컴포넌트가 HTML의 어디에서 렌더링됐는지 확인 가능
+    // 누르는 즉시 크롬 개발 도구의 메뉴 중 하나인 요소 탭으로 즉시 이동 해당 컴포넌트가 렌더링한 HTML 요소가 선택되는것을 볼 수 있다
+
+    // 벌레
+    // 클릭하는 순간 콘솔 탭에 해당 컴포넌트의 정보가 console.log를 실행해 기록
+    // 개발 도구 화면에서 보기에는 복잡한 정보를 확인하거나 해당 정보를 복사하는 등의 용도로 확인하고 싶다면 클릭 후 콘솔 탭 확인
+    // 해당 컴포넌트가 받는 props, 컴포넌트 내부에서 사용하는 hooks, 컴포넌트의 HTML 요소인 nodes가 기록
+
+    // 소스코드
+    // 해당 컴포넌트의 소스코드를 보여준다
+}
+
+{
+    // 오른쪽 props
+    // 해당 컴포넌트가 받은 props 확인 가능 
+    // 단순 원시값뿐만 아니라 함수도 포함
+    // 오른쪽 클릭 -> Copy value to clipboard = 클립보드 복사
+    // 오른쪽 클릭 -> Store as global variable = window.$r에 복사
+    
+    // Store as global variable을 선택하고 콘솔로 이동하면 해당 변수에 대한 정보가 담겨 있는 것을 볼 수 있다
+
+    // 값이 함수인 props를 누르면 Go to definition이 나타나는데 이를 클릭하면 해당 함수가 선언된 코드로 이동
+    // 값을 더블클릭시 수정 가능
+}
+
+{
+    // 오른쪽 hooks 
+    // use는 생략해서 나온다 (useState -> State)
+
+    // 개발자 모드에서 볼 수 있는 훅 목록
+    // State: useState
+    // Reducer: useReducer
+    // Context: useContext
+    // Memo: useMemo
+    // Callback: useCallback
+    // Ref: useRef
+    // id: useId
+    // LayoutEffect: useLayoutEffect
+    // Effect: useEffect
+    // 리액트 제공 훅이 아닌 경우 사용자 정의 훅도 use를 생략하고 보여줌 (useCounter -> Counter)
+}
+
+{
+    // 훅도 익명 함수 대신 기명 함수로 넘겨주면 해당 훅을 실행할 때 실행되는 함수의 이름을 확인 가능
+    // 익명함수는 f () {} 정도로만 확인할 수 있다
+    
+    //before
+    useEffect(() => {
+        console.log('useEffect')
+    })
+
+    // after
+    // Effect: f effectOnlyMount() {}
+    useEffect(function effectOnlyMount(){
+        console.log('useEffect')
+    })
+
+    // useEffect에는 익명 함수를 인수로 넘겨주기 때문에 여러 개 선언돼 있다면 어떤 훅인지 구별하기 어렵다
+    // hooks도 props와 마찬가지로 값을 더블클릭해 원하는 값으로 수정 가능
+}

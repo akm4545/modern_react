0eb36c03b3b4306fef474b080b0e544273af76d2
akm4545 <akm4545@naver.com>
2024-05-20 17:00:41 +0900

    chapter10

--- chapter10/react17.ts
+++ chapter10/react17.ts
@@ -179,4 +179,151 @@
     }
 
     ReactDOM.render(<App />, document.getElementById('root'))
+
+    // 이 코드는 리액트 16에서는 document에 달려 있으므로 stopPropagation이 의미가 없지만 리액트 17의 경우에는 컴포넌트 루트에 달려 있으므로 
+    // document에 부착한 console 이벤트를 볼 수 없다
+}
+
+{
+    // 리액트16에서는 JSX 변환을 위해서 코드 내에서 React 사용 구문이 없더라도 import React from 'react'가 필요했다
+    // 리액트17부터는 바벨과 협력해 이러한 import 구문이 없어도 JSX를 변환할 수 있게 됐다
+    // 이런 변화로 인해 불필요한 import 구문을 삭제해 번틀링 크기를 약간 줄일 수 있고 컴포넌트 작성을 더욱 간결하게 해준다
+
+    // 리액트 16 JSX 변환
+    const Component = (
+        <div>
+            <span>hello world</span>
+        </div>
+    )
+
+    // 리액트 16 변환
+    var Component = React.createElement(
+        'div',
+        null,
+        React.createElement('span', null, 'hello world'),
+    )
+
+    // JSX 코드를 변환시켜주지만 React.createElement를 수행할 때 필요한 import React from 'react'까지는 추가해 주지 않는다
+
+    // 리액트 17 변환
+    'use strict'
+
+    ver _jsxRuntime = require('react/jsx-runtime')
+
+    var Component = (0, _jsxRuntime.jsx)('div', {
+        children: (0, _jsxRuntime.jsx)('span', {
+            children: 'hello world',
+        }),
+    })
+
+    // JSX를 변환 시 필요한 모듈인 react/jsx-runtime을 불러오는 require 구문도 같이 추가되므로 import를 작성하지 않아도 된다
+}
+
+{
+    // 이벤트 풀링 제거
+    // 리액트 16에서는 이벤트 풀링이라 불리는 기능이 있었다
+    // 리액트에는 이벤트를 처리하기 위한 SynthticEvent라는 이벤트가 있었는데 브라우저의 기본 이벤트를 한 번 더 감싼 이벤트 객체다
+    // 리액트는 브라우저 기본 이벤트가 아닌 한번 래핑한 이벤트를 사용하기 때문에 이벤트가 발생할 때마다 이 이벤트를 새로 만들어야 했고 그 과정에서 
+    // 메모리 할당 작업이 일어났다
+    // 메모리 누수를 방지하기 위해 이렇게 만든 이벤트를 주기적으로 해제해야 하는 번거로움도 있다
+    // 이벤트 풀링은 SyntheticEvent 풀을 만들어서 이벤트가 발생할 때마다 가져오는 것을 의미한다
+
+    // 이벤트 풀링 시스템에서의 이벤트 발생
+    // 1. 이벤트 핸들러가 이벤트 발생
+    // 2. 합성 이벤트 풀에서 합성 이벤트 객체에 대한 참조를 가져옴
+    // 3. 이 이벤트 정보를 합성 이벤트 객체에 넣음
+    // 4. 유저가 지정한 이벤트 리스너 실행
+    // 5. 이벤트 객체 초기화 후 다시 이벤트 풀로 돌아감
+
+    // 합성 이벤트를 풀에서 꺼내 반복 사용 가능해서 효과적으로 보이지만 풀에서 이벤트를 받아오고 종료되면 다시 초기화(null)하는 방식은 사용하는 
+    // 쪽에서는 직관적이지 않다
+
+    export default function App(){
+        const [value, setValue] = useState('')
+        function handleChange(e: ChangeEvent<HTMLInputElement>){
+            setValue(() => {
+                return e.target.value
+            })
+        }
+
+        return <input onChange={handleChange} value={value}><input/>
+    }
+
+    // 이 코드는 에러를 발생시킨다
+    // 이벤트 핸들러롤 호출한 SyntheticEvent는 이후 재사용을 위해 null로 초기화 된다
+    // 따라서 비동기 코드 내부에서 SynthticEvent인 e에 접근하면 이미 사용되고 초기화된 이후이기 때문에 null만 얻게 된다
+    // 비동기 코드 내부에서 이 합성 이벤트 e에 접근하기 위해서는 추가적인 작업인 e.persist() 같은 처리가 필요했다
+
+    function handleChange(e: ChangeEvent<HTMLInputElement>){
+        e.persist()
+        setValue(() => {
+            return e.target.value
+        })
+    }
+}
+
+{
+    // useEffect 클린업 함수의 비동기 실행
+    // 리액트 16에서는 useEffect에 있는 클린업 함수는 동기적으로 처리됐다
+    // 동기 실행이기 때문에 다른 작업을 방해하므로 불필요한 성능 저하로 이어지는 문제가 존재했다
+
+    // 리액트 17에서는 화면이 완전히 업데이트된 이후에 클린업 함수가 비동기적으로 실행된다
+    // 클린업 함수는 컴포넌트의 커밋 단계가 완료될 때까지 지연된다 
+    // 이로써 약간의 성능적인 이점을 볼 수 있게 됐다
+
+    import React, {useState, Profiler, useEffect, useCallback} from 'react'
+
+    export default function App() {
+        const callback = useCallback(
+            (id, phase, actualDuration, baseDuration, startTime, commitTime) => {
+                console.log(phase)
+                console.table({ id, phase, commitTime})
+                console.groupEnd()
+            },
+            [],
+        )
+
+        return (
+            <Profiler id="React16" onRender={callback}>
+                <Button>
+                    <Users />
+                </Button>
+            </Profiler>
+        )
+    }
+
+    function Button({ children }){
+        const [toggle, setToggle] = useState(false)
+
+        const handleClick = useCallback(() => setToggle((prev) => !prev), [])
+
+        return (
+            <section>
+                <button onClick={handleClick}>{toggle ? 'show' : 'hide'}</button>
+                {toggle && children}
+            </section>
+        )
+    }
+
+    function Users() {
+        const [users, setUsers] = useState(null)
+
+        useEffect(() => {
+            const abortController = new AbortController()
+            const signal = abortController.signal
+
+            fetch('https://jsonplaceholder.typicode.com/users', {signal: signal})
+                .then((results) => results.json())
+                .then((data) => {
+                    setUsers(data)
+                })
+
+            return () => {
+                console.log('cleanup!')
+                abortController.abort()
+            }
+        }, [])
+
+        return <p>{users === null ? 'Loading' : JSON.stringify(users)}</p>
+    }
 }
